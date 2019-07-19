bc2e5fb538944efbd05817d4fc5d1359c793a64a
nixollassps <48063997+nixollassps@users.noreply.github.com>
2019-07-19 16:39:57 -0300

    close #3
    Formulário de Recuperação de senha

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
         <title>Sistema de Login TNX Systems</title>
+         <style>
+    #caixaCadastro,
+    #caixaRecuperarSenha,
+    #alerta{
+        display: none;
+    }
+
+    </style>
     </head>
 <body class="bg-dark">
     <main class="container mt-4">
@@ -73,7 +81,7 @@
         <!-- Formulário de Cadastro -->
 
         <section class="row mb-5">
-            <div class="col-lg-4 offset-4 bg-light rounded" id"caixaCadastro">
+            <div class="col-lg-4 offset-4 bg-light rounded" id="caixaCadastro">
             <h2 class="text-center">Cadastro de novo Usuário</h2>
             <form action="#" class="p-2" id="frmCadastro">
                     <div class="form-group">
@@ -115,12 +123,63 @@
                 </form>
             </div>
         </section>
-    
+    <!-- Formulário de recuperação de Senha --> 
+        <section class="row mb-5">
+        <div class="col-lg-4 offset-lg-4 bg-light runded" id="caixaRecuperarSenha">
+        <h2 class="text-center">Gerar nova senha</h2>
+        <form action="#" id="fromSenha">
+            <div class="form-group">
+                <small class="text-muted">
+            Para gerar uma nova senha, digite seu e-mail e receba as instruções
+                </small>
+            </div>
+            <div class="form-group">
+                <input type="email" name="emailSenha" id="emailSenha" class="form-control" placeholder="E-mail" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value=" :: Enviar E-mail ::" id="btnEnviarEmail" class="btn btn-primary btn-block">
+            </div>
+            <div class="form-group float right">
+                <a href="#" id="btnVoltar">Voltar</a>
+            </div>
+
+
+        </div>
+        
+        </section>
 
     </main>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+         /* Jquery*/ 
+    $(function(){
+        //Trocar da tela de login para recuperar Senha
+        $("#btnEsqueci").click(function(){
+            $("#caixaLogin").hide();
+            $("#caixaRecuperarSenha").show();
+        });
+        //Voltar para a tela de login
+        $("#btnVoltar").click(function(){
+            $("#caixaRecuperarSenha").hide();
+            $("#caixaLogin").show();
+
+        })
+        //Trocar de tela de login para cadastro de usuário
+            $("#btnCadastrar").click(function(){
+               $("#caixaLogin").hide();
+               $("#caixaCadastro").show();
+            });
+        //Voltar para a Tela de Login
+        $("#btnJáCadastro").click(function(){
+            $("#caixaLogin").show();
+            $("#caixaCadastro").hide();
+        })
+    });
+
+</script>
+
 </body>
 </html>
