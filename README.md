c025f02fdab5cfe72eebb9183519b89fbc9713e8
Nicollas PdS <nicollas_souza@estudante.sc.senai.br>
2019-08-23 16:46:29 -0300

    Recepção de dados para Cadasro de usuário

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
+##aula 05 - 23/08/2019
+Criação do banco de dados e ^~cadastro de usuários
 ## aula04 - 15/08/2019
  Recepção de dados no Back-end
 ## aula03 - 08/08/2019
--- recebe.php
+++ recebe.php
@@ -1,9 +1,46 @@
-
 <?php
-/**
- * Back-end
- * aula04 15/08/2019 - Recepção de dados no Back-end
- */
-echo "<pre>\n";
-print_r($_POST);
-echo "\n</pre>\n";
+//É necessário fazer a conexão com o Banco de Dados
+require_once "configDB.php";
+
+function verificar_entrada($entrada)
+{
+    $saida = trim($entrada); //Remove espaços antes e depois
+    $saida = stripslashes($saida);
+    $saida = htmlspecialchars($saida);
+    return $saida;
+}
+
+
+if (
+    isset($_POST['action']) &&
+    $_POST['action'] == 'cadastro'
+) {
+    echo "<h1>Dados enviados</h1>";
+    $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+    $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+    $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+    $senhaUsuario = verificar_entrada($_POST['senhaConfirma']);
+    $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
+    $concordar = $_POST['concordar'];
+    $dataCriacao = date("Y-m-d H:i:s");
+
+    $senha = sha1($senhaUsuario);
+    $senhaC = sha1($senhaConfirma);
+
+    echo "<h5>Nome completo: $nomeCompleto</h5>";
+    echo "<h5>Nome Usuário: $nomeUsuario</h5>";
+    echo "<h5>E-mail Usuário: $emailUsuario</h5>";
+    echo "<h5>Senha Usuário: $senhaUsuario</h5>";
+    echo "<h5>Senha Confirma: $senhaConfirma</h5>";
+    echo "<h5>Confirma: $concordar</h5>";
+    echo "<h5>Data de Criação: $dataCriacao</h5>";
+    if ($senha != $senhaC) {
+        echo "<h1>As senhas não conferem</h1>";
+        exit();
+    } else {
+        echo "<h5>Senha Codificada: $senha</h5>";
+    }
+} else {
+    echo "<h1 style='color:red'>Esta página nõ pode
+ser acessada diretamente</h1>";
+}
