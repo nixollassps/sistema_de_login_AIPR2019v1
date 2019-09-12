1e377921f636b63bafce9718fb691fd1bacc51d3
Nicollas Patrick MD FACS <nicollas_souza@estudante.sc.senai.br>
2019-09-12 16:39:11 -0300

    Preparação para gerar nova senha
    #12

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Set-2019 às 16:44
+-- Tempo de geração: 12-Set-2019 às 16:37
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -38,19 +38,20 @@ CREATE TABLE `usuario` (
   `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `senha` char(40) COLLATE utf8mb4_bin NOT NULL,
   `dataCriacao` datetime NOT NULL,
-  `avatar` varchar(200) COLLATE utf8mb4_bin NOT NULL
+  `avatar` varchar(200) COLLATE utf8mb4_bin NOT NULL,
+  `token` char(10) COLLATE utf8mb4_bin NOT NULL,
+  `tempo_de_vida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar`) VALUES
-(1, 'Nicollas Patrick', 'Nixollassps', 'souza.nih@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 13:38:40', ''),
-(2, 'Wanda Maximoff', 'Scarlet Witch', 'SW@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:07:30', 'https://i.ytimg.com/vi/izgx5Ln1i7c/maxresdefault.jpg'),
-(3, 'Wanda Maximoff', 'Wandinha Fodona', 'wm@soufoda.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:22:33', 'https://www.screengeek.net/wp-content/uploads/2018/05/avengers-infinity-war-scarlet-witch.jpg'),
-(4, 'Wanda Maximoff', 'Wanda', 'WandaEscarlate@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:24:27', 'https://www.screengeek.net/wp-content/uploads/2018/05/avengers-infinity-war-scarlet-witch.jpg'),
-(5, 'Wanda Maximoff', 'ScarletWanda', 'swm@gmail.com', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', '2019-09-06 16:34:32', 'https://a.wattpad.com/useravatar/-TaintedTragedy-.256.113099.jpg');
+INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar`, `token`, `tempo_de_vida`) VALUES
+(1, 'Nicollas Patrick', 'Nixollassps', 'souza.nih@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 13:38:40', '', '', '2019-09-12 19:33:18'),
+(2, 'Wanda Maximoff', 'Scarlet Witch', 'SW@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-06 16:07:30', 'https://i.ytimg.com/vi/izgx5Ln1i7c/maxresdefault.jpg', '', '2019-09-12 19:33:18'),
+(6, 'Nicollas P', 'NicoGato', 'nico@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2019-09-12 14:41:21', 'https://pa1.narvii.com/7132/f79a69dd373541b479afc14ae1dd594265e805edr1-312-132_128.gif', '', '2019-09-12 19:33:18'),
+(7, 'Bart Simpsons', 'barts', 'bart@gmail.com', 'bf8759494480995e23af85ae51c170512c6e99f2', '2019-09-12 16:35:47', 'https://i.pinimg.com/originals/00/69/85/0069850a178ceae7eab44095156cab99.png', '', '2019-09-12 19:35:47');
 
 --
 -- Índices para tabelas despejadas
@@ -71,7 +72,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
+##aula 07 - 12/09/2019
+Gerando uma nova senha
 ##aula 06 - 30/08/2019
 Cadastros de Usuários
 ##aula 05 - 23/08/2019
